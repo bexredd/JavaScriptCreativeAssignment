0307149f2a7fde7fa2a1c034007c0a3099096f64
bexredd <r.redd94@gmail.com>
2017-09-19 18:52:15 +0000

    adding script stuff

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
+var InitialX = 0;
+var InitialY = 0;
+var mouseDown = false;
 
+var mouseClicked = function(event){
+	mouseDown = true;
+	InitialX = event.clientX;
+	InitialY = event.clientY;
+  console.log(InitialX);
+}
 
+var moveImage = function(event){
+	if(mouseDown)
+  {
+  	var photoID =  event.currentTarget.id;
+  	document.getElementById(photoID).style.top="40px";
+  }
+}
 
-
-
-
-
-
-
-
+var mouseUp = function(){
+	mouseDown = false;
+}
 
 var onClickInput = function() {
 	document.getElementById("img_src").value = '';
