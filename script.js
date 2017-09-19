5d34da5b16ba384b6cec493051a7bdb9486f239f
Jacob Church <c.jacob.church@gmail.com>
2017-09-19 18:53:16 +0000

    script change

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var onClickInput = function() {
 }
 
 var addImage = function() {
-	let img = '<img src="';
+  let img = '<img src="';
   img += document.getElementById("img_src").value;
   img += '"/>';
   //add code to make this image appear in the middle of the screen(?)
