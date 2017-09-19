41ec23d5d82384bbd493aa60b937b4e99e99be7e
Jacob Church <c.jacob.church@gmail.com>
2017-09-19 18:56:31 +0000

    more style for new images

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var onClickInput = function() {
 var addImage = function() {
   let img = '<img src="';
   img += document.getElementById("img_src").value;
-  img += '"/>';
+  img += '" class="new_img"/>';
   //add code to make this image appear in the middle of the screen(?)
   document.getElementById("img_window").innerHTML += img;
 }
