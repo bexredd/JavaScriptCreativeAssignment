7010f0b8fe967f57caf6f3a35cc0f43e896c2266
bexredd <r.redd94@gmail.com>
2017-09-20 04:05:01 +0000

    moving the picture is all working

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 var InitialX = 0;
 var InitialY = 0;
-var mouseDown = false;
+var InitialTop = 0;
+var InitialLeft = 0;
+var EndTop = 0;
+var EndLeft = 0;
 
-var mouseClicked = function(event){
-	
-  if(mouseDown == false)
-  {mouseDown = true;
-	InitialX = event.clientX;
-	InitialY = event.clientY;
-  console.log(InitialX);}
-}
+var PictureIDnum = 1;
 
-var moveImage = function(event){
-	if(mouseDown)
-  {
-  	var photoID =  event.currentTarget.id;
-    
-    var XtoAdd = event.clientX - InitialX;
-    var YtoAdd = event.clientY - InitialY;
-    //initial - current + top;
-  	var imgTop = document.getElementById(photoID).style.top;
-    var imgLeft = document.getElementById(photoID).style.left;
-    if(imgTop=="")
-    {
-        console.log("if statement");
-    		document.getElementById(photoID).style.top=XtoAdd +"px";
-        document.getElementById(photoID).style.left=YtoAdd+"px";
+var dragStart = function(event) {
+  console.log("dragstart");
+  InitialX = event.clientX;
+  InitialY = event.clientY;
 
-    }
-    else
-    {
-    		var top = parseInt(imgTop, 10);
-        var left = parseInt(imgLeft, 10);
-        XtoAdd +=top;
-        YtoAdd += left;
-   			console.log(XtoAdd);
-        document.getElementById(photoID).style.top=XtoAdd +"px";
-        document.getElementById(photoID).style.left=YtoAdd+"px";
-    }
+  var photoID = event.currentTarget.id;
+  var top = document.getElementById(photoID).style.top;
+  var left = document.getElementById(photoID).style.left;
+  
+  if (top == "" && left == "") {
+    InitialTop = 0;
+    InitialLeft = 0;
+  } else {
+      InitialTop = parseFloat(top,10);
+    	InitialLeft = parseFloat(left,10);
   }
 }
 
-var mouseUp = function(){
-	mouseDown = false;
+var drag = function(event) {
+  var photoID = event.currentTarget.id;
+  var XtoAdd = event.clientX - InitialX + InitialLeft;
+  var YtoAdd = event.clientY - InitialY + InitialTop;
+
+  document.getElementById(photoID).style.top = YtoAdd + "px";
+  document.getElementById(photoID).style.left = XtoAdd + "px";
+
+  if (YtoAdd <= 100 + EndTop && YtoAdd >= EndTop - 100) {
+    EndTop = YtoAdd;
+    EndLeft = XtoAdd;
+  }
 }
+
+var dragEnd = function(event) {
+  var photoID = event.currentTarget.id;
+  var top = document.getElementById(photoID).style.top;
+  document.getElementById(photoID).style.top = EndTop + "px";
+  document.getElementById(photoID).style.left = EndLeft + "px";
+  InitialTop = EndTop;
+  InitialLeft = EndLeft;
+
+  console.log(top);
+}
+
+
+
 
 
 var onClickInput = function() {
-	document.getElementById("img_src").value = '';
+  document.getElementById("img_src").value = '';
 }
 
 var addImage = function() {
+  PictureIDnum = PictureIDnum + 1;
+  console.log(PictureIDnum);
   let img = '<img src="';
   img += document.getElementById("img_src").value;
-  img += '" class="new_img"/>';
+  img += ' "id = "image' + PictureIDnum + '"';
+  img += '" class="new_img" ';
+  img += 'ondragStart="dragStart(event)" onDrag="drag(event)" ondragEnd="dragEnd(event)"' + '/>';
   //add code to make this image appear in the middle of the screen(?)
   document.getElementById("img_window").innerHTML += img;
 }
